0ad87fcf64199b3ab2d793170f59f450aa32d009
Mizuki <ne191217@senshu-u.jp>
2020-12-13 14:44:58 +0000

    Customerのシードデータ投入

---
+++ db/migrate/20201203071009_create_customers.rb
@@ -0,0 +1,13 @@
+class CreateCustomers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :customers do |t|
+      t.string :full_name
+      t.string :user_name, null: false
+      t.string :address
+      t.string :tel_num
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
