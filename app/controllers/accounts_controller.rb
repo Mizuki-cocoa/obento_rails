48a5db42aa218bef8ec93e8c1f4d943101a6be09
Mizuki <ne191217@senshu-u.jp>
2020-12-29 05:24:19 +0000

    ログインしたアカウントについて内容編集ができる

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,20 @@
+class AccountsController < ApplicationController
+  before_action :login_required
+  def show
+    @customer= current_customer
+  end
+
+  def edit
+    @customer= current_customer
+  end
+
+  def update
+    @customer=current_customer
+    @customer.assign_attributes(params[:account])
+    if @customer.save
+      redirect_to :account, notice: "アカウント情報を更新しました。"
+    else
+      render "edit"
+    end
+  end
+end
