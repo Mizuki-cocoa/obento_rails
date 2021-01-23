b9ff35ae2489da9654b530ef274bdc23c3924221
Mizuki <ne191217@senshu-u.jp>
2021-01-23 13:24:57 +0000

    新規会員調整

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -13,13 +13,14 @@ class AccountsController < ApplicationController
 
   def create
     @customer=Customer.new(params[:customer])
+    @customer.assign_attributes(params[:customer])
     if @customer.save
       cookies.signed[:customer_id]={value: @customer.id}
       @cart=Cart.new(id: @customer.id, customer_id: @customer.id)
       @cart.save
       redirect_to :root, notice: "会員登録が完了しました。"
     else
-      render "/customers/new"
+      render new_customer_path
     end
   end
 
